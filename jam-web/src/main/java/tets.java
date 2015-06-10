53ba89c5f100cf1715424cd7a3a51b5d2a946ac2
jinn <bettertj@outlook.com>
2015-06-10 20:29:02 +0800

    jquery源码分析。

--- jam-web/src/main/java/tets.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by TJ on 2015/5/17.
- */
-public class tets {
-
-}
---
+++ jam-web/web/test/myjq.js
@@ -0,0 +1,17 @@
+/**
+ * Created by jinn on 2015/6/5.
+ */
+
+
+//函数与立即执行
+(function (arg) {
+    console.log( "1:" +  typeof arg);
+})(window);
+
+
+//函数立即执行
+(function (arg) {
+
+    console.log( "2:" + typeof arg);
+
+}(window));
